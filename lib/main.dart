752e07c92323ac9c525e7aa8da8ad3b240ed0558
alex <49459824+alexx-ngn@users.noreply.github.com>
2025-04-24 20:51:33 -0400

    Fixed dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,14 @@ import 'screens/profile_screen.dart';
 import 'screens/notifications_screen.dart';
 import 'screens/settings_screen.dart';
 
-void main() {
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
