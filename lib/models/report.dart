b8ce425fddc9da4ad962f1a78111b0addd7d16d2
ryandong05 <dongr1118@gmail.com>
2025-05-06 17:33:36 -0400

    update report model for admin

--- lib/models/report.dart
+++ lib/models/report.dart
@@ -64,4 +64,100 @@ class Report {
       'resolvedAt': resolvedAt != null ? Timestamp.fromDate(resolvedAt!) : null,
     };
   }
+}
+
+class PostReport {
+  final String id;
+  final String postId;
+  final String reporterId;
+  final String reportedUserId;
+  final String postContent;
+  final String reporterUsername;
+  final String reportedUsername;
+  final String reason;
+  final DateTime timestamp;
+
+  PostReport({
+    required this.id,
+    required this.postId,
+    required this.reporterId,
+    required this.reportedUserId,
+    required this.postContent,
+    required this.reporterUsername,
+    required this.reportedUsername,
+    required this.reason,
+    required this.timestamp,
+  });
+
+  factory PostReport.fromMap(Map<String, dynamic> map) {
+    return PostReport(
+      id: map['id'] ?? '',
+      postId: map['postId'] ?? '',
+      reporterId: map['reporterId'] ?? '',
+      reportedUserId: map['reportedUserId'] ?? '',
+      postContent: map['postContent'] ?? '',
+      reporterUsername: map['reporterUsername'] ?? '',
+      reportedUsername: map['reportedUsername'] ?? '',
+      reason: map['reason'] ?? '',
+      timestamp: (map['timestamp'] as Timestamp).toDate(),
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'postId': postId,
+      'reporterId': reporterId,
+      'reportedUserId': reportedUserId,
+      'postContent': postContent,
+      'reporterUsername': reporterUsername,
+      'reportedUsername': reportedUsername,
+      'reason': reason,
+      'timestamp': Timestamp.fromDate(timestamp),
+    };
+  }
+}
+
+class UserReport {
+  final String id;
+  final String reportedUserId;
+  final String reporterId;
+  final String reportedUsername;
+  final String reporterUsername;
+  final String reason;
+  final DateTime timestamp;
+
+  UserReport({
+    required this.id,
+    required this.reportedUserId,
+    required this.reporterId,
+    required this.reportedUsername,
+    required this.reporterUsername,
+    required this.reason,
+    required this.timestamp,
+  });
+
+  factory UserReport.fromMap(Map<String, dynamic> map) {
+    return UserReport(
+      id: map['id'] ?? '',
+      reportedUserId: map['reportedUserId'] ?? '',
+      reporterId: map['reporterId'] ?? '',
+      reportedUsername: map['reportedUsername'] ?? '',
+      reporterUsername: map['reporterUsername'] ?? '',
+      reason: map['reason'] ?? '',
+      timestamp: (map['timestamp'] as Timestamp).toDate(),
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'reportedUserId': reportedUserId,
+      'reporterId': reporterId,
+      'reportedUsername': reportedUsername,
+      'reporterUsername': reporterUsername,
+      'reason': reason,
+      'timestamp': Timestamp.fromDate(timestamp),
+    };
+  }
 } 
